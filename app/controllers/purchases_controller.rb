bd6c10a8a2caf845932ff9779ccc708ef74167ac
Lubosky <Lubosky@users.noreply.github.com>
2015-09-13 09:50:54 +0200

    Fixed ActiveRecord problem in purchases_controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,7 @@ class PurchasesController < ApplicationController
 
 	def show
 		@purchase = Purchase.find_by_uuid(params[:id])
-		@product = Product.find_by(@purchase.product_id)
+		@product = Product.find(@purchase.product_id)
 	end
 
 end
